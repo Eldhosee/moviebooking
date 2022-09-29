1498995d67fd5ceabf7ad19ec0a2cc6bf96bf962
unknown <eldhoseiypeparavatty@gmail.com>
2022-09-29 11:56:09 +0530

    commit

--- mini_project/settings.py
+++ mini_project/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-njq1_%d#2x6##3m&e11^#4h5fn76((bb-7q982!fdpgrxq9w^&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000/']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
